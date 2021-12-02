7b68b2dc90aafd4d6b1f82679a8b1cd33b08c556
Furkan TUNÇ <furkant88@outlook.com>
2021-12-02 11:03:49 +0300

    1. Ders

---
+++ src/app.js
@@ -0,0 +1,12 @@
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+const euroDun = 11.20 //sabit değer bunu değiştiremiyoruz
+
+console.log(dolarDun)
+console.log(euroDun)
+
+let konutKrediler = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+for(let i = 0; i<konutKrediler.length; i++){
+    console.log(konutKrediler[i])
+}
